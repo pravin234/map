782beaf797cc49c47dd3d63ecde7241ef7f082d7
pravin234 <pravinkadam234@gmail.com>
2024-04-02 14:10:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const x = "23";
 if (x === 23) console.log(x);
+Registration complete
+Your API key is: 88796c7631fb422a9bef6b8e2ecacefa
+
+For help getting started please look at our getting started guide.
+
+We post API status updates and other news on our Twitter feed, so please follow us there if that's important to you:
+
